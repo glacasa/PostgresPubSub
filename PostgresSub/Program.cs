ef7d80425acf4fe90fd494aebffb07c0f439f099
Guillaume Lacasa <glacasa@users.noreply.github.com>
2023-03-09 09:37:48 +0100

    PostgresqlListener should be singleton and not transient

--- PostgresSub/Program.cs
+++ PostgresSub/Program.cs
@@ -25,7 +25,7 @@ var builder = new HostBuilder()
     })
     .ConfigureServices((ctx, services) =>
     {
-        services.AddTransient<PostgresqlListener>();
+        services.AddSingleton<PostgresqlListener>();
     })
     .UseConsoleLifetime();
 
